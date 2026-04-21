2d21ad1e4ce73fa50ccc8d7e5ce732162f589a8c
pietrobertino <pietrobertino20@gmail.com>
2026-04-21 12:58:39 +0200

    connect front end to back end

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 require('dotenv').config(); //per usare il .env, ricordarsi anche di installare la libreria dotenv
 
+const cors = require('cors');
 const express = require('express');
 const app = express();
 const port = process.env.PORT;
 const movieRouter = require('./routers/movies');
 const errorsHandler = require('./middlewares/errorsHandler');
 const notFound = require('./middlewares/NotFound');
+
+const corsOptions = {
+    origin: process.env.FRONT_END_URL,
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
     res.send('Movies server');
